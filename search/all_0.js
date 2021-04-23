bd679143b0bb057e9f53b93beb5ad26b6a9d0b9e
Delt06 <Delt06@users.noreply.github.com>
2021-04-23 20:08:34 +0000

    deploy: 76075363318aa84a86b4575456d6ef029b9d3443

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_3cimg_20src_3d_22packages_2fcom_2edeltation_2edi_2dframework_2fassets_2fdeltation_2fdiframework_2feditor_2fgraphics_2fresolver_2epng_22_20alt_3d_22logo_22_20title_3d_22logo_22_2f_3e_20unity_20di_20framework_0',['&lt;img src=&quot;Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Editor/Graphics/resolver.png&quot; alt=&quot;Logo&quot; title=&quot;Logo&quot;/&gt; Unity DI Framework',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['_3cimg_20src_3d_22packages_2fcom_2edeltation_2edi_2dframework_2fassets_2fdeltation_2fdiframework_2feditor_2fgraphics_2fresolver_2epng_22_20alt_3d_22logo_22_20title_3d_22logo_22_2f_3e_20unity_20di_20framework_0',['&lt;img src=&quot;Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Editor/Graphics/resolver.png&quot; alt=&quot;Logo&quot; title=&quot;Logo&quot;/&gt; Unity DI Framework',['../index.html',1,'']]]
 ];
