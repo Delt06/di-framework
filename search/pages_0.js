3a309e5a35682885d0760f27363294f0ed5038ed
Delt06 <Delt06@users.noreply.github.com>
2021-04-23 20:13:47 +0000

    deploy: 26a581477cebac6e927b6ed6b6394b93217b61c0

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_3cimg_20src_3d_22packages_2fcom_2edeltation_2edi_2dframework_2fassets_2fdeltation_2fdiframework_2feditor_2fgraphics_2fresolver_2epng_22_20alt_3d_22logo_22_20title_3d_22logo_22_2f_3e_20unity_20di_20framework_221',['&lt;img src=&quot;Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Editor/Graphics/resolver.png&quot; alt=&quot;Logo&quot; title=&quot;Logo&quot;/&gt; Unity DI Framework',['../index.html',1,'']]]
+  ['_3cimg_20src_3d_22https_3a_2f_2fgithub_2ecom_2fdelt06_2fdi_2dframework_2fblob_2fmaster_2fpackages_2fcom_2edeltation_2edi_2dframework_2fassets_2fdeltation_2fdiframework_2feditor_2fgraphics_2fresolver_2epng_3fraw_3dtrue_22_20alt_3d_22logo_22_20title_3d_22logo_22_2f_3e_20unity_20di_20framework_221',['&lt;img src=&quot;https://github.com/Delt06/di-framework/blob/master/Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Editor/Graphics/resolver.png?raw=true&quot; alt=&quot;Logo&quot; title=&quot;Logo&quot;/&gt; Unity DI Framework',['../index.html',1,'']]]
 ];
