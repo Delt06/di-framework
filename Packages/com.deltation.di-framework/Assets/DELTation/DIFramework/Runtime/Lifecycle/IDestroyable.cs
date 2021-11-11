80509fdc4f2c0a376b12ec2abaabbf81b339b34b
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-11-11 23:21:56 +0300

    Add IStartable, IUpdatable, and IDestroyable [closes #66]

---
+++ Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Runtime/Lifecycle/IDestroyable.cs
@@ -0,0 +1,12 @@
+﻿namespace DELTation.DIFramework.Lifecycle
+{
+    public interface IDestroyable
+    {
+        /// <summary>
+        ///     Executed by
+        ///     <see cref="DELTation.DIFramework.ContainerLifecycle" />
+        ///     at Unity's OnDestroy callback.
+        /// </summary>
+        void OnDestroy();
+    }
+}
---
+++ Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Runtime/Lifecycle/IUpdatable.cs
@@ -0,0 +1,12 @@
+﻿namespace DELTation.DIFramework.Lifecycle
+{
+    public interface IUpdatable
+    {
+        /// <summary>
+        ///     Executed by
+        ///     <see cref="DELTation.DIFramework.ContainerLifecycle" />
+        ///     at Unity's Update callback.
+        /// </summary>
+        void OnUpdate();
+    }
+}
