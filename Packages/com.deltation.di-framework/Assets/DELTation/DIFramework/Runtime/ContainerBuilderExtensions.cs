4e409490ac25f4da6edc0f64ea38ef800e36b3a7
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-11-11 21:44:03 +0300

    Add container builder extensions: RegisterIfNotNull and TryResolveAndRegister<T>() [closes #65]

---
+++ Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Runtime/ContainerBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine.Assertions;
+using Object = UnityEngine.Object;
+
+namespace DELTation.DIFramework
+{
+    /// <summary>
+    ///     Utility methods for registering dependencies in ContainerBuilders.
+    /// </summary>
+    public static class ContainerBuilderExtensions
+    {
+        /// <summary>
+        ///     Register the given dependency only if it is not null (it checks for Unity null too).
+        /// </summary>
+        /// <param name="containerBuilder">Container builder to register dependency in.</param>
+        /// <param name="obj">Registered object.</param>
+        /// <returns>The same container builder.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="containerBuilder" /> is null.</exception>
+        [NotNull]
+        public static ContainerBuilder RegisterIfNotNull([NotNull] this ContainerBuilder containerBuilder,
+            [CanBeNull] object obj)
+        {
+            if (containerBuilder == null) throw new ArgumentNullException(nameof(containerBuilder));
+            if (IsNullOrUnityNull(obj)) return containerBuilder;
+
+            Assert.IsNotNull(obj);
+            return containerBuilder.Register(obj);
+        }
+
+        private static bool IsNullOrUnityNull([CanBeNull] object obj) =>
+            obj is Object unityObj ? unityObj == null : obj == null;
+
+        /// <summary>
+        ///     Try to resolve a dependency globally. If resolved, register it.
+        /// </summary>
+        /// <param name="containerBuilder">Container builder to register dependency in.</param>
+        /// <typeparam name="T">Type of dependency to try to resolve.</typeparam>
+        /// <returns>The same container builder.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="containerBuilder" /> is null.</exception>
+        [NotNull]
+        public static ContainerBuilder TryResolveGloballyAndRegister<T>(
+            [NotNull] this ContainerBuilder containerBuilder) where T : class
+        {
+            if (containerBuilder == null) throw new ArgumentNullException(nameof(containerBuilder));
+            return Di.TryResolveGlobally(out T service) ? containerBuilder.Register(service) : containerBuilder;
+        }
+    }
+}
