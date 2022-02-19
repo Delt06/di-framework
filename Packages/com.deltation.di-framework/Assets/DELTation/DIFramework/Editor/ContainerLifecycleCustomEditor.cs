90f2c78659cfafbbee45c5016eb1244d34c193ac
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-02-19 23:17:58 +0300

    Add custom editor for ContainerLifecycle

---
+++ Packages/com.deltation.di-framework/Assets/DELTation/DIFramework/Editor/ContainerLifecycleCustomEditor.cs
@@ -0,0 +1,62 @@
+﻿#if ODIN_INSPECTOR
+using EditorBase = Sirenix.OdinInspector.Editor.OdinEditor;
+#else
+using EditorBase = UnityEditor.Editor;
+#endif
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace DELTation.DIFramework.Editor
+{
+    [CustomEditor(typeof(ContainerLifecycle), true)]
+    public class ContainerLifecycleCustomEditor : EditorBase
+    {
+        private bool _foldout;
+        private GUIStyle _labelStyle;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!Application.isPlaying) return;
+
+            _foldout = EditorGUILayout.Foldout(_foldout, "Affected Objects");
+            if (!_foldout) return;
+
+            _labelStyle = new GUIStyle(GUI.skin.label)
+            {
+                richText = true,
+            };
+
+            var containerLifecycle = (ContainerLifecycle) target;
+            EditorGUI.indentLevel++;
+            DrawObjectsList("<b>IStartable</b>", containerLifecycle.Startables);
+            DrawObjectsList("<b>IUpdatable</b>", containerLifecycle.Updatables);
+            DrawObjectsList("<b>IFixedUpdatable</b>", containerLifecycle.FixedUpdatables);
+            DrawObjectsList("<b>ILateUpdatable</b>", containerLifecycle.LateUpdatables);
+            DrawObjectsList("<b>IDestroyable</b>", containerLifecycle.Destroyables);
+            EditorGUI.indentLevel--;
+        }
+
+        private void DrawObjectsList(string headerLabel, IReadOnlyList<object> objects)
+        {
+            if (objects.Count == 0) return;
+            Label(headerLabel);
+            EditorGUI.indentLevel++;
+
+            for (var index = 0; index < objects.Count; index++)
+            {
+                var startable = objects[index];
+                Label(startable.GetType().GetFriendlyName());
+            }
+
+            EditorGUI.indentLevel--;
+        }
+
+        private void Label(string label)
+        {
+            EditorGUILayout.LabelField(label, _labelStyle);
+        }
+    }
+}
